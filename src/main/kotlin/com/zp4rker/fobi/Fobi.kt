bf6ce22e1fbc41ddcd6bfc2861c4956109ba3e4c
zp4rker <me@zp4rker.com>
2019-11-27 10:35:36 +1100

    changed online status to dnd

--- src/main/kotlin/com/zp4rker/fobi/Fobi.kt
+++ src/main/kotlin/com/zp4rker/fobi/Fobi.kt
@@ -3,6 +3,7 @@ package com.zp4rker.fobi
 import com.zp4rker.core.discord.config.JsonFile
 import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.hooks.AnnotatedEventManager
 
@@ -11,7 +12,8 @@ val config = JsonFile.loadOrDefault("config.json", true)
 fun main() {
     JDABuilder(AccountType.BOT).setToken(config.getString("token")).run {
         setEventManager(AnnotatedEventManager())
-        setActivity(Activity.listening("IN DEVELOPMENT"))
+        setActivity(Activity.listening("dev instructions"))
+        setStatus(OnlineStatus.DO_NOT_DISTURB)
         build()
     }
 }
